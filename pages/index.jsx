97c953db1f170bcccd5f0e0c5e090c62e89d128f
Ahmad Rifqy Syarwani <arifqys@gmail.com>
2021-05-29 17:24:55 +0700

    add apollo client

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,12 @@
 import { Box, Heading, Text } from '@chakra-ui/react';
 import Head from 'next/head';
 import Image from 'next/image';
+import PropTypes from 'prop-types';
+import { gql } from '@apollo/client';
+// eslint-disable-next-line sort-imports
+import client from '../apollo-client';
 
-const Home = () => (
+const Home = ({ posts }) => (
   <div className="container">
     <Head>
       <title>Ahmad Rifqy Syarwani</title>
@@ -26,6 +30,18 @@ const Home = () => (
       </Text>
     </Box>
 
+    <Box mb="10">
+      <Heading as="h2" size="xl" mb="5">
+        Blog (in Bahasa)
+      </Heading>
+      {
+        posts.map((post) => (
+          <Heading as="h2" size="md" mb="3" key={post.id}>{post.title}</Heading>
+        ))
+      }
+
+    </Box>
+
     <footer>
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -41,5 +57,30 @@ const Home = () => (
     </footer>
   </div>
 );
+
+Home.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export const getStaticProps = async () => {
+  const GET_BLOG_POSTS = gql`
+    query GetBlogs {
+        allBlogs {
+          title
+          id
+        }
+      }
+  `;
+
+  const { data: posts } = await client.query({
+    query: GET_BLOG_POSTS,
+  });
+
+  return {
+    props: {
+      posts: posts.allBlogs,
+    },
+  };
+};
 
 export default Home;
