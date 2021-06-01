16fbceee710707cb76dd5c0057d71147a958e4b2
Ahmad Rifqy Syarwani <arifqys@gmail.com>
2021-06-01 17:48:04 +0700

    add p page home and single post

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import '../styles/globals.css';
+import 'nprogress/nprogress.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }) => (
   <ChakraProvider>
-    <Component {...pageProps} />
+    <div className="container">
+      <Component {...pageProps} />
+    </div>
   </ChakraProvider>
 );
 
