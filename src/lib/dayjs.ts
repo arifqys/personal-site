8a88c3fd3aa2afbd9730b66d5f4cdd3b490e04e3
Ahmad Rifqy Syarwani <arifqys@gmail.com>
2022-11-17 16:25:10 +0700

    init astro

--- src/lib/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import 'dayjs/locale/id';
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-
-dayjs.locale(`id`);
-dayjs.extend(relativeTime);
-
-export default dayjs;
